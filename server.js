43f3d070a2545d86e357ee1ce0aefcb5e0188d00
DaanTacoSchool <djmessen@avans.nl>
2017-12-11 19:49:33 +0100

    HTTP/HTTPS SUPPORT

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ app.use(logger('dev'));
 // CORS headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN ||  process.env.ALLOW_ORIGIN_HTTPS || 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*' || 'http://localhost:4200');
+   // res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN ||  process.env.ALLOW_ORIGIN_HTTPS || 'http://localhost:4200');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
